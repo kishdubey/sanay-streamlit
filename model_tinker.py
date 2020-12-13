2a1d2d0c831b0c81e475eff13b044b9c3192bb60
Kish Dubey <dkish2000@gmail.com>
2020-12-13 00:05:02 -0500

    added large keras models and pickle file to lfs

--- model_tinker.py
+++ model_tinker.py
@@ -4,26 +4,25 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-
 def predict(message):
-    model=load_model(‘movie_sent.h5’)
-    with open(‘tokenizer.pickle’, ‘rb’) as handle:
-    tokenizer = pickle.load(handle)
+    model=load_model('keras/model.h5')
+    with open('keras/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
     x_1 = tokenizer.texts_to_sequences([message])
     x_1 = pad_sequences(x_1, maxlen=300)
     predictions = model.predict(x_1)[0][0]
     return predictions
 
-st.title(“Movie Review Sentiment Analyzer”)
-message = st.text_area(“Enter Review”,”Type Here ..”)
+st.title("Sentiment Analyzer")
+message = st.text_area("Enter Text","Type Here ..")
 
-if st.button(“Analyze”):
- with st.spinner(‘Analyzing the text …’):
- prediction=predict(message)
- if prediction > 0.6:
- st.success(“Positive review with {:.2f} confidence”.format(prediction))
- st.balloons()
- elif prediction <0.4:
- st.error(“Negative review with {:.2f} confidence”.format(1-prediction))
- else:
- st.warning(“Not sure! Try to add some more words”)
+if st.button("Predict"):
+ with st.spinner('Analyzing the text …'):
+     prediction=predict(message)
+     if prediction >= 0.6:
+         st.success(f"Positive! With {round(prediction*100, 2)}% confidence")
+         st.balloons()
+     elif prediction <= 0.4:
+         st.error(f"Negative! With {round(prediction*100, 2)}% confidence")
+     else:
+         st.warning("Not sure man ¯\_(ツ)_/¯")
